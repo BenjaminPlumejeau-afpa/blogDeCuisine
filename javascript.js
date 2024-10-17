4ee6cecef176678844a64f7e2b52edbdd850c236
BenjaminPlumejeau-afpa <benjamin.plumejeau@afpa-dev-pompey.fr>
2024-10-17 17:00:56 +0200

    Champs de recherche + début du menu déroulant mobile

--- javascript.js
+++ javascript.js
@@ -1,3 +1,52 @@
+//  --- CONSTANTES ---
+const recipes = [
+    {
+        id: 1,
+        title: "Tarte aux pommes",
+        image: "images/photoTartePommes.jpg",
+        imageAlt: "Photo de notre délicieuse tarte aux pommes",
+        category: "Desserts",
+        difficulty: "Facile"
+    },
+    {
+        id: 2,
+        title: "Lasagne",
+        image: "https://placehold.co/600x400",
+        imageAlt: "Image placeholder",
+        category: "Plats principaux",
+        difficulty: "Moyen"
+    },
+    {
+        id: 3,
+        title: "Salade César",
+        image: "https://placehold.co/600x400",
+        imageAlt: "Image placeholder",
+        category: "Entrées",
+        difficulty: "Facile"
+    },
+    {
+        id: 4,
+        title: "Forêt noire",
+        image: "https://placehold.co/600x400",
+        imageAlt: "Image placeholder",
+        category: "Desserts",
+        difficulty: "Difficile"
+    },
+    {
+        id: 5,
+        title: "Gratin Dauphinois",
+        image: "https://placehold.co/600x400",
+        imageAlt: "Image placeholder",
+        category: "Plats principaux",
+        difficulty: "Moyen"
+    }
+];
+
+const recipesCollection = document.getElementById("recipes");
+const categoriesCollection = document.getElementById("categories");
+const searchField = document.getElementById("search");
+
+
 //  --- FONCTIONS ---
 
 // fonction affichant les recettes dans la page
@@ -44,18 +93,19 @@ function filterByCategory(event) {
     if (event.target.nodeName === "LI") {
         let categoryList = document.querySelectorAll("#categories li");
         let recipeList = document.querySelectorAll("#recipes article");
-        // au clique sur tout voir on affiche toutes les recettes sans filtre
+        // au clique sur "tout voir" on affiche toutes les recettes sans filtre
         if (event.target.textContent === "Tout voir") {
             // modification de l'affichage des filtres
             categoryList.forEach(element => {
                 element.classList.remove("activeCategory");
                 event.target.classList.add("activeCategory");
             });
+            searchField.value = "";
             // Affichage de toutes les recettes
             recipeList.forEach(element => {
                 element.classList.remove("masque");
             });
-        // au clique sur un autre filtre on applique ce dernier
+            // au clique sur un autre filtre on applique ce dernier
         } else {
             // modification de l'affichage des filtres
             categoryList.forEach(element => {
@@ -64,66 +114,42 @@ function filterByCategory(event) {
             });
             // Affichage de toutes les recettes correspondant au filtre
             recipeList.forEach(element => {
+                //if (element.textContent.includes(event.target.textContent)) {
                 if (element.textContent.includes(event.target.textContent)) {
                     element.classList.remove("masque");
                 } else {
                     element.classList.add("masque");
                 }
             });
+            searchRecipe();
         }
+    }
+}
+
+// fonction permettant de rechercher une recette par son nom
+function searchRecipe() {
+    if (searchField.value !== ""); {
+        let searched = searchField.value.toUpperCase();
+        recipesCollection.childNodes.forEach(element => {
+            if (element.nodeName === "ARTICLE") {
+                let recipeTitle = element.childNodes[1].textContent.toUpperCase();
+                if (recipeTitle.includes(searched) && !(element.classList.contains("masque"))) {
+                    element.classList.remove("masque");
+                } else {
+                    element.classList.add("masque");
+                }
+            }
+        });
     }
 }
 
 
 //  --- PROGRAMME PRINCIPAL ---
-const recipes = [
-    {
-        id: 1,
-        title: "Tarte aux pommes",
-        image: "images/photoTartePommes.jpg",
-        imageAlt: "Photo de notre délicieuse tarte aux pommes",
-        category: "Desserts",
-        difficulty: "Facile"
-    },
-    {
-        id: 2,
-        title: "Lasagne",
-        image: "https://placehold.co/600x400",
-        imageAlt: "Image placeholder",
-        category: "Plats principaux",
-        difficulty: "Moyen"
-    },
-    {
-        id: 3,
-        title: "Salade César",
-        image: "https://placehold.co/600x400",
-        imageAlt: "Image placeholder",
-        category: "Entrées",
-        difficulty: "Facile"
-    },
-    {
-        id: 4,
-        title: "Forêt noire",
-        image: "https://placehold.co/600x400",
-        imageAlt: "Image placeholder",
-        category: "Desserts",
-        difficulty: "Difficile"
-    },
-    {
-        id: 5,
-        title: "Gratin Dauphinois",
-        image: "https://placehold.co/600x400",
-        imageAlt: "Image placeholder",
-        category: "Plats principaux",
-        difficulty: "Moyen"
-    }
-];
 
-const recipesCollection = document.getElementById("recipes");
-const categoriesCollection = document.getElementById("categories");
-
-// affichage des recettes
+// affichage des recettes et catégories
 displayRecipes();
 displayCategories();
 
+// Ecouteurs
 categoriesCollection.addEventListener("click", filterByCategory);
+searchField.addEventListener("keypress", searchRecipe);
